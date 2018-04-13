9137570916de4799d9fcdefd82c26df4c32e9702
Leigh Halliday <leighhalliday@gmail.com>
2018-04-13 16:06:12 -0400

    Removing key out of code

--- src/services/unsplash.js
+++ src/services/unsplash.js
@@ -3,8 +3,7 @@ import axios from "axios";
 export default async term => {
   const response = await axios.get("https://api.unsplash.com/search/photos", {
     params: {
-      client_id:
-        "4070052047e85343f77f7bbfb056ca4da387e25b3114baff0644247779a29964",
+      client_id: process.env.REACT_APP_UNSPLASH_TOKEN,
       query: term
     }
   });
